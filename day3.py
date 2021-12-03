aeb39122630edb60c055836e93f0aacbc4af138f
Abdoulaye Ba <abba@f0r2s13.codam.nl>
2021-12-03 22:02:18 +0100

    to finish at home

--- day3.py
+++ day3.py
@@ -4,5 +4,12 @@ new_content = {}
 for i in range (len(content)):
     new_content[i] = content[i].split('0')
 k = 0
+for i in new_content:
+    if new_content[i][k] == '':
+        new_content[i][k] = 0;
+        k += 1
+    k = 0
+    
+print(new_content)
 
 print(new_content[0][2])
