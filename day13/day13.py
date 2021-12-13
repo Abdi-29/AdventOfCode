7de8e7d5f1af71f9240f186d9f136bf516b3d36a
Abdoulaye Ba <abba@f0r1s10.codam.nl>
2021-12-13 22:37:55 +0100

    to_fix

--- day13/day13.py
+++ day13/day13.py
@@ -1,36 +1,36 @@
 def main():
     paper = open('input.txt').read().splitlines()
     line = []
-    max_value = []
+    max_value = [1311, 895]
     for i in range(len(paper)):
         line.append([int(x) for x in paper[i].split(',')])
     # x_max = []
     # y_max = []
     # for i in line:
     #     x_max.append(i[0])
-    # #     y_max.append(i[1])
+    #     y_max.append(i[1])
     # print(max(x_max), max(y_max))
-    board = [['.' for x in range(1311)] for y in range(895)]
-    fill_the_board(line, board)
+    board = [['.' for x in range(max_value[0])] for y in range(max_value[1])]
+    fill_the_board(line, board, max_value)
 
-def fill_the_board(paper, board):
+def fill_the_board(paper, board, max_value):
     for x in range(len(paper)):
         for y in range(1):
             i = paper[x][1]
             j = paper[x][0]
             board[i][j] = '#'
-    board = fold_vert(board, 447, paper)
-    board = fold_horiz(board, 655, paper)
+    board = fold_vert(board, max_value[1], paper)
+    board = fold_horiz(board, max_value[0], paper)
     count = 0
-    # for line in board:
-    #     print(line[:5])
+    for line in board:
+        print(line[:5])
     for line in board:
         for j in line:
             if j == '.':
                 count += 1
 
 def fold_vert(board, v, paper):
-    for y in range(11):
+    for y in range(v):
         board[v][y] = '-'
     for i in paper:
         if i[1] > v:
@@ -40,16 +40,14 @@ def fold_vert(board, v, paper):
     return(board[:v])
 
 def fold_horiz(board, h, paper):
-    count = 0
     new_input = get_new_value(board)
-    for y in range(895):
+    print(new_input)
+    for y in range(7):
         board[y][h] = '|'
-    for i in paper: #new_input
+    for i in new_input:
         x = h - (i[0] - h)
         y = i[1]
         board[y][x] = '#'
-        count += 1
-    print(count)
     return(board[:])
 
 def get_new_value(board):
